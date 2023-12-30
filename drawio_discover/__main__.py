d7f4401b7191c21bdb12995cbe1a405bfc5964e6
Laurent Bousquet <laurent.bousquet@continental-corporation.com>
2023-12-30 15:22:32 +0100

    feat: main added to get code base to scan

--- drawio_discover/__main__.py
+++ drawio_discover/__main__.py
@@ -1,4 +1,19 @@
 """ Main module for the application. """
 
+import argparse
+
+
+def run():
+    """Main function for the application."""
+    parser = argparse.ArgumentParser(
+        prog="drawio-discover",
+        description="Help to populate drawio files to make diagrams easily",
+        epilog=""" This avoid to scan all folders manually to retrieve code base information. """,
+    )
+    parser.add_argument("input_folder", help="code base to scan", type=str)
+    args = parser.parse_args()
+    print(f"Hello Drawio Discover! scan folder: {args.input_folder}")
+
+
 if __name__ == "__main__":
-    print("Hello Drawio Discover!")
+    run()
