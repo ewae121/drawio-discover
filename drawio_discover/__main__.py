f8c8467808992a30748d966c661d95ad0f17deb9
Laurent Bousquet <laurent.bousquet@continental-corporation.com>
2023-12-30 15:31:45 +0100

    feat: discoverer package added

--- drawio_discover/__main__.py
+++ drawio_discover/__main__.py
@@ -2,6 +2,8 @@
 
 import argparse
 
+from drawio_discover.discovrerer.discoverer import discover_packages
+
 
 def run():
     """Main function for the application."""
@@ -12,7 +14,10 @@ def run():
     )
     parser.add_argument("input_folder", help="code base to scan", type=str)
     args = parser.parse_args()
-    print(f"Hello Drawio Discover! scan folder: {args.input_folder}")
+
+    detected_packages = discover_packages(args.input_folder)
+    for package in detected_packages:
+        print(package)
 
 
 if __name__ == "__main__":
